10b0b7669cb811ed63c77e58f6bed5765e20b8fb
SamV-42 <vanderarks@brpsk12.org>
2017-11-25 23:44:00 -0500

    removed unnecessary import (2)

--- main.py
+++ main.py
@@ -2,21 +2,16 @@ import kivy
 
 from kivy.app import App
 
-from kivy.core.window import Window
-
 from kivy.graphics import *
 
 from kivy.uix.widget import Widget
 from kivy.uix.floatlayout import FloatLayout 
 from kivy.uix.label import Label
-from kivy.uix.dropdown import DropDown
 from kivy.uix.button import Button
 from kivy.uix.behaviors import DragBehavior
-
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
 from kivy.uix.popup import Popup
 from kivy.uix.spinner import Spinner
 
